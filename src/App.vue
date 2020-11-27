8b9bdd45de0978bbb12ffea393d5e7c43556b6d5
Tank <wanjunqing@aliyun.com>
2020-11-27 15:10:54 +0800

    初始化Vue3项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="box"></div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+}
+</script>
+<style lang="less" scoped>
+  #box{
+    height: 200px;
+    background-color: #bfa;
+  }
+</style>
